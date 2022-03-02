01667cf5b08d382140a51923f621de6f440e6810
Matt Berteaux <mattyb678@gmail.com>
2022-03-01 20:54:30 -0700

    Add pom and inital src files, update gitignore

---
+++ src/main/java/xyz/mattyb/unikode/Unikode.kt
@@ -0,0 +1,12 @@
+package xyz.mattyb.unikode
+
+class Unikode {
+
+    companion object {
+        @JvmStatic
+        fun test() {
+            println("Hello world")
+        }
+    }
+
+}
---
+++ src/test/java/xyz/mattyb/unikode/UnikodeTest.kt
@@ -0,0 +1,12 @@
+package xyz.mattyb.unikode
+
+import org.junit.jupiter.api.Test
+
+open class UnikodeTest {
+
+    @Test
+    fun test() {
+        Unikode.test()
+    }
+
+}
